57633c37322afe106d10b514a01caf5972a6bf86
ER73 <eduardo-dragneel@hotmail.com>
2016-11-06 14:34:45 -0600

    Add files via upload

---
+++ La_Granja_22/Bicho3.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Bicho3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bicho3 extends Actor
+{
+    /**
+     * Act - do whatever the Bicho3 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
---
+++ La_Granja_22/Records.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Records here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Records extends Actor
+{
+    /**
+     * Act - do whatever the Records wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
